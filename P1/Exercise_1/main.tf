362491fe99c4c84ae4c73e806b3074b1e5655bf8
Kin Cheung <email_kin@yahoo.com>
2020-06-13 23:33:24 +0800

    I can't find this architecture.

--- P1/Exercise_1/main.tf
+++ P1/Exercise_1/main.tf
@@ -1,17 +1,15 @@
 # TODO: Designate a cloud provider, region, and credentials
 provider "aws" {
-  access_key = ""
-  secret_key = ""
   region = "us-west-2"
 }
 
 # TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
 resource "aws_instance" "Udacity_T2" {
-  count = 4
+  count = 1
   ami = "ami-0e34e7b9ca0ace12d"
   instance_type = "t2.micro"
   tags = {
-    Name = "T2 Server ${count.index}"
+    Name = "Udacity T2"
   }
 }
 
@@ -22,6 +20,6 @@ resource "aws_instance" "Udacity_M4" {
   ami = "ami-0e34e7b9ca0ace12d"
   instance_type = "m4.large"
   tags = {
-    Name = "M4 Server ${count.index}"
+    Name = "Udacity M4"
   }
 }
